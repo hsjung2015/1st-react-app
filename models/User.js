b0030f44719339d64f5e5d5fc82db7270c516144
hsjung2015 <hsj1002love@naver.com>
2021-02-17 14:27:32 +0900

    로그인 및 토큰 생성

--- models/User.js
+++ models/User.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
-
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
-
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({    //schema
     name:{
@@ -51,8 +50,34 @@ userSchema.pre('save', function(next){
                 next()
             })
         })
+    } else{
+        next()
     }
 })
+
+userSchema.methods.comparePassword = function(plainPassword, cb){
+    
+    //plainPassword 1234567     DB에 있는 암호화된 비밀번호 $2b$10$Zh6/Q6.c31EfEUogWLnz.u0DF1eUtChhVD4HWb8f9JbLZJ8JpTDfu
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if(err) return cb(err),
+        cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(cb) {
+    var user = this;
+    // jsonwebtoken을 이용해서 token을 생성하기.
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+    // user._id + 'secretToken' = token
+    // ->
+    // 'secretToken' -> user._id
+
+    user.token = token
+    user.save(function(err, user){
+        if(err) return cb(err)
+        cb(null, user)
+    })
+}
 
 const User = mongoose.model('User', userSchema)
 
